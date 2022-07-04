a9ca411cb000be5e9e4cc90a7d264fc7087f356c
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-07-04 13:05:27 -0700

    alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
