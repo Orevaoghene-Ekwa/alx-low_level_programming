b1150a2ed21cd33bf3965dbe0748888d267d035b
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-07-04 14:06:45 -0700

    print single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints out single digits
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = '0'; numbers <= '9'; numbers++)
+	{
+		putchar(numbers);
+	}
+
+	putchar(\n);
+
+	return (0);
+}
