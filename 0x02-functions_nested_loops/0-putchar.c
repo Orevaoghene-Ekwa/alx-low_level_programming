ef3da74a945baf1a6f086a8f7d398ff5475680cd
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-07-05 14:20:14 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * main - returns the string _putchar
+ *
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char s;
+
+	s = "_putchar";
+	putchar("%c\n", s);
+
+	return (0);
+}
