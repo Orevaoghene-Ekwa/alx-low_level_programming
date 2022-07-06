1625cedd495c9cd6773685a67e757f33d7a47f60
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-07-06 15:48:54 -0700

    teest

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,11 @@
 void print_alphabet_x10(void)
 {
 	int number;
+	char alphabet;
 
 	number = 0;
 	while (number < 10)
 	{
-		char alphabet;
-
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
