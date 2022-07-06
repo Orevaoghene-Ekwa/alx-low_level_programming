31e59d671dd61e2dde0e7cdf518ff9e6c2aa5248
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-07-06 14:47:49 -0700

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ *
+ * Return: always 0
+ */
+void print_alphabet(void);
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
