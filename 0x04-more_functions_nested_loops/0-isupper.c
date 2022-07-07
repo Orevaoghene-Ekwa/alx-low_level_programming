1c5c79f9e51b8b312c8252dfa686a1f54b6a1414
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-07-07 15:40:34 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * _isupper - ckecks for uppercase characters
+ * @c: variable to be tested
+ * Return: 1 if uppercase, 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A'; c <= 'Z' c++)
+		return (1);
+	else
+		return (0);
+}
