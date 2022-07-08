4bb151e1d12feca812fe3a01822712fd7c224241
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-07-07 17:32:14 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ * print_most_numbers - print numbers except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9' n++)
+	{
+		if (n == 2)
+			continue;
+		else if (n == 4)
+			continue;
+		else
+			_putchar(n);
+	}
+	_putchar('\n');
+}
