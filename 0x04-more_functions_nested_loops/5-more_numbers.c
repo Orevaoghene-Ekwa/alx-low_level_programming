84051ba3f5432103c1f6d1ac4fb2bf833d1637cf
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-07-08 11:20:22 -0700

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,14 +12,14 @@ void more_numbers(void)
 	int n;
 
 	i = 0;
-	while (i <= 9)
+	for (i = 0; 1 <= 9; i++)
 	{
-		i++;
 		for (n = 0; n <= 14; n++)
 		{
-			if (i <= 10)
-				_putchar(n);
-			_putchar('\n');
+			if (n > 9)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
+	_putchar('\n');
 	}
 }
