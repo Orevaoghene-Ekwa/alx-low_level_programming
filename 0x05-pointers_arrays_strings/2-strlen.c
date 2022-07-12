853e96f1ebeb480c8a140534c8c9b6c329836eaa
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-07-12 15:35:43 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ *
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+
+	return (index);
+}
