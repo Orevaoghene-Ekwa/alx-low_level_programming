7c7c584dab8f33e9d27de4f019da849dd88d40c2
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-08-02 02:05:37 -0700

    srtuct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef my_dog
+#define my_dog
+
+/**
+ * struct dog - dog description
+ * @name: name of dog
+ * @age: dog's name
+ * @owner: Name of dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
