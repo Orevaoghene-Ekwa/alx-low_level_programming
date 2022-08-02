019b685474151393356c9149e5c3ebd8a59b5970
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-08-02 07:59:33 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
+	for (i = 0; n > 0; i++)
 	{
-		*(s + i) = b;
-		i++;
+		s[i] = b;
+		n -= 1;
 	}
 	return (s);
 }
