74336a22fbd4ce8090f4e82da789816575dbc7b3
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-08-03 06:22:10 -0700

    strpn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * _strspn - gets the length of a prefix substring
  * @s: pointer to a char
  * @accept: pointer to a char
- * Retun: s
+ * Return: j
  */
 
 unsigned int _strspn(char *s, char *accept)
