fbef2188c14eb2e3ed5adf23a9dbb5db2f7f4a2f
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2023-08-10 20:02:30 +0100

    simplified input to putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int number;
 
-	for (number = 48; number <= 57; number++)
+	for (number = 0; number <= 9; number++)
 	{
-		putchar(number);
+		putchar(number + '0');
 
-		if (number == 57)
+		if (number == 9)
 		{
 			break;
 		}
