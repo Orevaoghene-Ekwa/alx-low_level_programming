c1adf95af5f815e68661b5134339dec524ccbbaf
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2024-01-27 04:03:41 -0800

    rerun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,20 +4,18 @@
 
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
+ * Return: Always 0 (Success)
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
 {
 
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 
 
