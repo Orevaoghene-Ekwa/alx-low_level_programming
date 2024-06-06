202908ec3982682bec028d958dae9fcc90fac310
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2024-06-06 14:43:34 -0700

    a function to create another dog struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include "dog.c"
+#include <stdio.h>
+
+/**
+ * new_dog -creates a new dog
+ *
+ * @name: name of the dog
+ * @age: Age of dog
+ * owner: Owner of dog's name
+ * @p: pointer to dog_t struct
+ *
+ * Return: p
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	/*allocating memory for the struct*/
+	p = malloc(sizeof(dog_t));
+
+	if (p == NULL)
+		return (NULL);
+	if (name == NULL)
+	{
+		free(p);
+		free(owner);
+		return(NULL);
+	}
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return(NULL)
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return(p);
+}
