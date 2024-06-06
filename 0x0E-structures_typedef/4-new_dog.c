5644ba3d2a33ca5eedfbbede39601f129193cc5c
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2024-06-06 15:10:53 -0700

    updating funtion to return null

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,22 +19,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/*allocating memory for the struct*/
 	p = malloc(sizeof(dog_t));
 
-	if (p == NULL)
-		return (NULL);
-	if (name == NULL)
+	if (p != NULL)
 	{
-		free(p);
-		free(owner);
-		return (NULL);
+		if (name == NULL)
+		{
+			free(p);
+			free(owner);
+			return (NULL);
+		}
+		if (owner == NULL)
+		{
+			free(p);
+			free(name);
+			return (NULL);
+		}
+		p->name = name;
+		p->age = age;
+		p->owner = owner;
 	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
+	else
 		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
 }
