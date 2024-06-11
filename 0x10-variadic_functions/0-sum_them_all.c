2093dea1ef83ff771cbf1722400e2651cf00a0c3
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2024-06-11 00:00:39 -0700

    variadic sum function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * sum_them_all - sums all its arguments
+ * @n: first argument
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int i, sum = 0;
+
+	va_start(args, n);
+
+	while (i < n)
+	{
+		sum += va_arg(args, int);
+		i++;
+	}
+	va_end;
+	return (sum);
+}
